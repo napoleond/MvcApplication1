458d86493a9423a2d20351025543c22fb7801b0c
napoleond <dave.noel@gmail.com>
2014-06-03 03:32:33 -0700

    Some really dinky tests

---
+++ ClassLibrary1/ConsoleWorkerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using NUnit.Framework;
+using ConsoleApplication1;
+
+
+[TestFixture]
+public class ConsoleWorkerTest
+{
+    [Test]
+    public void MockedIFacesWork()
+    {
+        //this is obviously a pretty useless test, just shows that interfaces abstract out network calls
+        MockStore store = new MockStore();
+        var message = store.queue.GetMessage();
+        Assert.AreEqual(null, message);
+    }
+}
---
+++ ClassLibrary1/VisitorsControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using NUnit.Framework;
+using MvcApplication1.Models;
+using MvcApplication1.Controllers;
+
+[TestFixture]
+public class VisitorsControllerTest
+{
+    //create mocks
+    public class MockCommandAgent : ICommandAgent
+    {
+        public void SendCommand(object command)
+        {
+            //do nothing; a more interesting mock might build the list in memory
+            //to test adding items
+        }
+    }
+
+    public class MockListFactory : IListFactory<Visitor>
+    {
+        public IEnumerable<Visitor> GetList()
+        {
+            //again, a more interesting mock might actually return some data
+            return new List<Visitor>();
+        }
+    }
+
+    private VisitorsController controller;
+
+    [SetUp]
+    public void Init()
+    {
+        MockCommandAgent mockCommander = new MockCommandAgent();
+        MockListFactory mockListFactory = new MockListFactory();
+        controller = new VisitorsController(mockCommander, mockListFactory);
+    }
+
+    [Test]
+    public void DoesReturnViewItem()
+    {
+        var result = controller.Index() as ViewResult;
+        Assert.AreEqual("Index", result.ViewName);
+    }
+}
